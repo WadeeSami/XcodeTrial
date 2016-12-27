7e0bf5581dab1970988e2af9f1d50e20412ef0ae
Wadee Sami <wabuzant@endeavorpal.com>
2016-12-27 11:59:30 +0200

    Initial Commit

---
+++ testVersionControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testVersionControl
+//
+//  Created by Wadee Sami on 12/27/16.
+//  Copyright © 2016 Wadee AbuZant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
